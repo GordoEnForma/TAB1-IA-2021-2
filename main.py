75b8c59aa8c358f5413549cf9f6845c17e9f482a
cledmir <56932765+cledmir@users.noreply.github.com>
2021-09-18 17:39:48 -0500

    pygame

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 # GENES = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,20,21,22,23,24,25,26,27,28,29,30]
 
-TARGET = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
+int TARGET = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
 # TARGET = [random.randint(1,Q_GENES) for _ in range(16)]
 Q_GENES = 1000
 POPULATION_SIZE = 1000
@@ -67,73 +67,121 @@ def generate_population():
 
 def run(population,flag,generacion):
     done = False
+    aux = True
     fuente = pygame.font.Font(None,30)
+
+    font = pygame.font.Font(None, 32)
+    clock = pygame.time.Clock()
+    input_box = pygame.Rect(100, 100, 140, 32)
+    color_inactive = pygame.Color('lightskyblue3')
+    color_active = pygame.Color('dodgerblue2')
+    color = color_inactive
+    active = False
+    text = ''
+    done = False
     while not done:
         ## PYGAME
 
-        if not flag:
-            population = sorted(population, key = lambda x:x.puntuacion,reverse=True)
-            # for i in population:
-            #     print(i.puntuacion,i.cromosoma)
-            # flag = True
+        if aux:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # If the user clicked on the input_box rect.
+                    if input_box.collidepoint(event.pos):
+                        # Toggle the active variable.
+                        active = not active
+                    else:
+                        active = False
+                    # Change the current color of the input box.
+                    color = color_active if active else color_inactive
+                if event.type == pygame.KEYDOWN:
+                    if active:
+                        if event.key == pygame.K_RETURN:
+                            Q_GENES = int(text)
+                            aux = False
+                        elif event.key == pygame.K_BACKSPACE:
+                            text = text[:-1]
+                        else:
+                            text += event.unicode
 
-            #Condición de Parada
-            if population[0].puntuacion == len(TARGET):
-                flag = True
+            screen.fill((30, 30, 30))
+            # Render the current text.
+            txt_surface = font.render(text, True, color)
+            # Resize the box if the text is too long.
+            width = max(200, txt_surface.get_width()+10)
+            input_box.w = width
+            # Blit the text.
+            screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+            # Blit the input_box rect.
+            pygame.draw.rect(screen, color, input_box, 2)
+
+            pygame.display.flip()
+            clock.tick(30)
+        else:
+            if not flag:
+                population = sorted(population, key = lambda x:x.puntuacion,reverse=True)
+                # for i in population:
+                #     print(i.puntuacion,i.cromosoma)
+                # flag = True
+
+                #Condición de Parada
+                if population[0].puntuacion == len(TARGET):
+                    flag = True
 
 
-            # De lo contrario, se generan nuevos descendientes para la nueva generación
-            nueva_cepa = []
-            # Realizar elitismo, lo que significa que el 10% de la población más apta
-            # pasa a la siguiente generación
-            s = int((10*POPULATION_SIZE)/100)
-            nueva_cepa.extend(population[:s])
+                # De lo contrario, se generan nuevos descendientes para la nueva generación
+                nueva_cepa = []
+                # Realizar elitismo, lo que significa que el 10% de la población más apta
+                # pasa a la siguiente generación
+                s = int((10*POPULATION_SIZE)/100)
+                nueva_cepa.extend(population[:s])
 
-            # Del 50% de la población más apta, los individuos
-            # se aparearán para producir descendencia
-            s = int((90*POPULATION_SIZE)/100)
-            for _ in range(s):
-                parent1 = random.choice(population[:50])
-                parent2 = random.choice(population[:50])
-                # print('Parent 1',parent1.cromosoma,end="\n")
-                # print('Parent 2',parent2.cromosoma,end="\n")
+                # Del 50% de la población más apta, los individuos
+                # se aparearán para producir descendencia
+                s = int((90*POPULATION_SIZE)/100)
+                for _ in range(s):
+                    parent1 = random.choice(population[:50])
+                    parent2 = random.choice(population[:50])
+                    # print('Parent 1',parent1.cromosoma,end="\n")
+                    # print('Parent 2',parent2.cromosoma,end="\n")
 
-                child = parent1.reproduccion(parent2)
-                nueva_cepa.append(child)
-            population = nueva_cepa
+                    child = parent1.reproduccion(parent2)
+                    nueva_cepa.append(child)
+                population = nueva_cepa
 
-            # time.sleep(1.5)
-            print(f'Generacion: {generacion}, Poblacion: {population[0].cromosoma},Puntuacion: {population[0].puntuacion}')
+                # time.sleep(1.5)
+                print(f'Generacion: {generacion}, Poblacion: {population[0].cromosoma},Puntuacion: {population[0].puntuacion}')
 
-            generacion += 1
+                generacion += 1
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        screen.fill(WHITE)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+            screen.fill(WHITE)
 
-        for x in range(100,WIDTH, 100):
-            pygame.draw.line(screen, BLACK,(x, 0), (x,HEIGHT), 2)
-        for y in range(100, HEIGHT, 100):
-            pygame.draw.line(screen, BLACK,(0, y), (WIDTH, y), 2)
-        
-        
-        # str(population[0].cromosomas[i])
-        i = 0
+            for x in range(100,WIDTH, 100):
+                pygame.draw.line(screen, BLACK,(x, 0), (x,HEIGHT), 2)
+            for y in range(100, HEIGHT, 100):
+                pygame.draw.line(screen, BLACK,(0, y), (WIDTH, y), 2)
 
-        # for y in range(50, HEIGHT, 100):
-        #     for x in range(50,WIDTH, 100):
-        #         texto = fuente.render(str(population[0].cromosoma[int((((x/50)-1)/2)+(l*((y/50)-1)/2))]),0,(200,60,80))
-        #         screen.blit(texto,((x,y)))
 
-        # int(population[0].cromosomas[i])
+            # str(population[0].cromosomas[i])
+            i = 0
 
-        # int((((x/50)-1)/2)+(l*((y/50)-1)/2))
+            # for y in range(50, HEIGHT, 100):
+            #     for x in range(50,WIDTH, 100):
+            #         texto = fuente.render(str(population[0].cromosoma[int((((x/50)-1)/2)+(l*((y/50)-1)/2))]),0,(200,60,80))
+            #         screen.blit(texto,((x,y)))
 
-        for y in range(50, HEIGHT, 100):
-            for x in range(50,WIDTH, 100):
-                texto = fuente.render(str(population[0].cromosoma[int((((x/50)-1)/2)+(l*((y/50)-1)/2))]),0,(200,60,80))
-                screen.blit(texto,((x,y)))
+            # int(population[0].cromosomas[i])
+
+            # int((((x/50)-1)/2)+(l*((y/50)-1)/2))
+
+            for y in range(50, HEIGHT, 100):
+                for x in range(50,WIDTH, 100):
+                    texto = fuente.render(str(population[0].cromosoma[int((((x/50)-1)/2)+(l*((y/50)-1)/2))]),0,(200,60,80))
+                    screen.blit(texto,((x,y)))
         pygame.display.flip()
 
     print(f'Generacion: {generacion}, Poblacion: {population[0].cromosoma},Puntuacion: {population[0].puntuacion}')
